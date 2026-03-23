7bf84ded2580d1bb3d4e83c6bd87fee92b06bbef
quynn <nnquy2610@gmail.com>
2026-03-23 10:12:28 +0700

    fix exam + practice management

--- app/controllers/management/exams_controller.rb
+++ app/controllers/management/exams_controller.rb
@@ -2,8 +2,26 @@ class Management::ExamsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_exam, only: [ :show, :edit, :update, :destroy ]
   def index
-    @exams = Exam.all
-    @exams = @exams.where(topic_id: Topic.where(course_id: Course.where(user_id: current_user.id))) if current_user&.teacher?
+    # Load topics available to the user (teachers see only their courses' topics)
+    if current_user&.teacher?
+      @topics = Topic.where(course_id: Course.where(user_id: current_user.id))
+    else
+      @topics = Topic.all
+    end
+
+    # Default selected topic = first available topic
+    @selected_topic = if params[:topic_id].present?
+                        @topics.find_by(id: params[:topic_id])
+    else
+                        @topics.first
+    end
+
+    # If no topics available, return empty set
+    if @selected_topic
+      @exams = Exam.where(topic_id: @selected_topic.id)
+    else
+      @exams = Exam.none
+    end
   end
 
   def show
@@ -11,11 +29,32 @@ class Management::ExamsController < ApplicationController
   end
 
   def create
-    @exam = Exam.new(exam_params)
-    if @exam.save
-      redirect_to new_management_lesson_path, notice: "Exam was successfully created."
+    # Support bulk create via params[:exams] (Option A)
+    if params[:exams].present?
+      created = []
+      ActiveRecord::Base.transaction do
+        params[:exams].each do |q|
+          # skip completely blank rows
+          next if q[:question].blank? && q[:answers].blank? && q[:correct_answers].blank?
+
+          created << Exam.create!(
+            topic_id: (q[:topic_id].presence || params[:topic_id].presence),
+            question: q[:question],
+            answers: q[:answers],
+            correct_answers: q[:correct_answers],
+            type: q[:type]
+          )
+        end
+      end
+
+      redirect_to management_exams_path, notice: "Tạo #{created.size} câu hỏi thành công"
     else
-      render :new
+      @exam = Exam.new(exam_params)
+      if @exam.save
+        redirect_to new_management_lesson_path, notice: "Exam was successfully created."
+      else
+        render :new
+      end
     end
   end
 
