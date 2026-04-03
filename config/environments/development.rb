2b8e165a45381926781cafae7a5ecb8cde7c009f
ngocquy2610 <nnquy2610@gmail.com>
2026-04-03 15:29:35 +0700

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            ENV["GMAIL_USERNAME"], # Use environment variables!
-    password:             ENV["GMAIL_PASSWORD"], # Use an "App Password," not your real login
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       "plain",
     enable_starttls_auto: true,
     openssl_verify_mode: 'none'
