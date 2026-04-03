71ef22050d6546ad37f3f3fa97485c2110aaabe8
ngocquy2610 <nnquy2610@gmail.com>
2026-04-03 15:27:11 +0700

    config confirmation mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # Set host to be used by links generated in mailer templates (AWS server IP).
+  config.action_mailer.default_url_options = { host: "13.210.19.168", protocol: "http" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
