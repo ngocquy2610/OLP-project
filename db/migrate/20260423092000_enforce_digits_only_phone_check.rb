49addcfeb52f9c6481ec465a39099f9948de4239
ngocquy2610 <nnquy2610@gmail.com>
2026-04-23 16:43:20 +0700

    fix phone validate

---
+++ db/migrate/20260423092000_enforce_digits_only_phone_check.rb
@@ -0,0 +1,19 @@
+class EnforceDigitsOnlyPhoneCheck < ActiveRecord::Migration[8.1]
+  def up
+    remove_check_constraint :users, name: "users_phone_format_check"
+
+    add_check_constraint :users,
+      "phone IS NOT NULL AND btrim(phone) <> '' AND char_length(phone) BETWEEN 10 AND 11 AND phone ~ '^\\d{10,11}$'",
+      name: "users_phone_format_check",
+      validate: false
+  end
+
+  def down
+    remove_check_constraint :users, name: "users_phone_format_check"
+
+    add_check_constraint :users,
+      "phone IS NOT NULL AND btrim(phone) <> '' AND char_length(phone) <= 11 AND phone ~ '^\\+?[0-9\\s\\-\\(\\)]{10,11}$'",
+      name: "users_phone_format_check",
+      validate: false
+  end
+end
