7f74b9244ac26ad7279b5b797de87531b96d633b
ngocquy2610 <nnquy2610@gmail.com>
2026-04-23 15:21:07 +0700

    hotfix topic model (rename exam_attempts)

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :course
   has_many :lessons, dependent: :destroy
   has_many :exams, dependent: :destroy
-  has_many :exams_attempts, dependent: :destroy
+  has_many :exam_attempts, dependent: :destroy
 
   validates :name, presence: true
 end
