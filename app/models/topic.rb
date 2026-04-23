75a4195daff5445ca932e0ba0479e6224984ef06
ngocquy2610 <nnquy2610@gmail.com>
2026-04-23 15:20:10 +0700

    hot fix model topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :course
   has_many :lessons, dependent: :destroy
   has_many :exams, dependent: :destroy
+  has_many :exams_attempts, dependent: :destroy
 
   validates :name, presence: true
 end
